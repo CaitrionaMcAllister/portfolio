68a582ea04e1e3f8b9d24866cc70d956ea917eaf
Caitríona McAllister <91537940+CaitrionaMcAllister@users.noreply.github.com>
2023-05-15 15:51:17 +0100

    adding links

--- src/App.js
+++ src/App.js
@@ -6,14 +6,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Soon to be Caitriona's Portfolio</p>
+        <p>Caitriona's Portfolio Coming Soon!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/catzcollective/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Enter Test
+          Check out my instagram in the meantime
+        </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/caitrionamcallister"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Let's chat on LinkedIn
         </a>
       </header>
     </div>
