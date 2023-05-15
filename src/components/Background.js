d5dcdb4953f32b5012b24a6fadf1ce9722837d80
Caitríona McAllister <91537940+CaitrionaMcAllister@users.noreply.github.com>
2023-05-15 23:22:14 +0100

    added clouds

--- src/components/Background.js
+++ src/components/Background.js
@@ -9,8 +9,12 @@ function Background() {
       <Cloud position={[10, 6, -15]} speed={0.2} opacity={0.25} />
       <Cloud position={[0, 10, -15]} speed={0.2} opacity={0.2} />
       <Cloud position={[0, -10, -15]} speed={0.2} opacity={0.2} />
-      <Cloud position={[-10, -6, -15]} speed={0.2} opacity={0.3} />
-      <Cloud position={[10, 6, -15]} speed={0.2} opacity={0.25} />
+      <Cloud position={[-20, -10, 10]} speed={0.2} opacity={0.3} />
+      <Cloud position={[10, 6, 5]} speed={0.2} opacity={0.25} />
+      <Cloud position={[-15, 10, 10]} speed={0.2} opacity={0.2} />
+      <Cloud position={[0, -10, -20]} speed={0.2} opacity={0.2} />
+      <Cloud position={[-10, -6, 5]} speed={0.2} opacity={0.3} />
+      <Cloud position={[10, 6, 10]} speed={0.2} opacity={0.25} />
     </group>
   );
 }
