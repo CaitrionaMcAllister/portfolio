64b3694369b7cdebe0aa6fc7b4479fd15aeb0041
caitriona.mcallister <caitriona.mcallister1@gmail.com>
2023-08-04 00:16:13 +0100

    feat: adding marque images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Header } from "../Overlay.js";
 import "../App.css";
+import Marquee from "react-fast-marquee";
+import img10 from '../components/websiteImagesHome/10.png'
+// import img from '../components/websiteImagesHome/2.png'
+
 // import { Canvas } from "@react-three/fiber";
 // import { ScrollControls, Scroll } from "@react-three/drei";
 
@@ -8,5 +12,10 @@ import "../App.css";
 export const Home = () => (
   <>
     <Header />
+    <Marquee direction="up" gradientWidth="200">
+      <div>
+        <img src={img10} alt=""/>
+      </div>
+    </Marquee>
   </>
 );
