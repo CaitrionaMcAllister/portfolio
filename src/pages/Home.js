b6d59aed7cd1033d83c1d33d5c8645dca919e80e
caitriona.mcallister <caitriona.mcallister1@gmail.com>
2023-08-04 00:36:00 +0100

    feat: marque adjustments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { Header } from "../Overlay.js";
 import "../App.css";
 import Marquee from "react-fast-marquee";
 import img10 from '../components/websiteImagesHome/10.png'
-// import img from '../components/websiteImagesHome/2.png'
+import img11 from '../components/websiteImagesHome/11.png'
 
 // import { Canvas } from "@react-three/fiber";
 // import { ScrollControls, Scroll } from "@react-three/drei";
@@ -12,9 +12,12 @@ import img10 from '../components/websiteImagesHome/10.png'
 export const Home = () => (
   <>
     <Header />
-    <Marquee direction="up" gradientWidth="200">
-      <div>
+    <Marquee direction="up" gradient="true">
+      <div className="imageWrapper">
         <img src={img10} alt=""/>
+      </div>
+      <div className="imageWrapper">
+        <img src={img11} alt=""/>
       </div>
     </Marquee>
   </>
